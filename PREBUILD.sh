fd3beeb2ed5b813b930515d013f4d3325fe42ae0
nullchinchilla <nullchinchilla@pm.me>
2025-09-11 14:06:19 -0600

    new

--- PREBUILD.sh
+++ PREBUILD.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export PATH=$HOME/.cargo/bin:/usr/bin/:/bin:$PATH
-export ANDROID_NDK_HOME=$HOME/Android/Sdk/ndk/27.0.12077973
+export ANDROID_NDK_HOME=$HOME/Android/Sdk/ndk/29.0.14033849
 
 # Make sure output directories exist
 mkdir -p prebuild/arm64-v8a prebuild/armeabi-v7a prebuild/x86 prebuild/x86_64
@@ -9,10 +9,10 @@ mkdir -p prebuild/arm64-v8a prebuild/armeabi-v7a prebuild/x86 prebuild/x86_64
   cd geph5/binaries/geph5-client
   export CARGO_TARGET_DIR=../../target/
   # Build for arm64-v8a
-  ~/.cargo/bin/cargo ndk -t arm64-v8a -p 21 build --release --features aws_lambda
+  ~/.cargo/bin/cargo ndk -t arm64-v8a --platform 21 build --release --features aws_lambda
 
   # Build for armeabi-v7a
-  ~/.cargo/bin/cargo ndk -t armeabi-v7a -p 21 build --release --features aws_lambda
+  ~/.cargo/bin/cargo ndk -t armeabi-v7a --platform 21 build --release --features aws_lambda
 )
 
 # Copy the resulting binaries to the correct folders
